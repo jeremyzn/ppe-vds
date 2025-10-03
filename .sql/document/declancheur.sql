1bb49fbf90bb50d0b349e3b97e8a81c9f39ce53d
Alexi MERCIER <amercier@stremi.fr>
2025-10-03 16:50:24 +0200

    Dernière sauvegarde le 03/10/2025 à 16:50

--- .sql/document/declancheur.sql
+++ .sql/document/declancheur.sql
@@ -1,42 +1,42 @@
-use ppe;
-
-drop trigger if exists avant_insert_document;
-drop trigger if exists avant_update_document;
-
-
-
-
-DELIMITER //
-CREATE TRIGGER avant_insert_document
-    BEFORE INSERT
-    ON document
-    FOR EACH ROW
-BEGIN
-    DECLARE v_max_id BIGINT;
-
-    -- Vérifie l'ID : si NULL ou existant, on prend max(id)+1
-    IF NEW.id IS NULL OR EXISTS (SELECT 1 FROM document WHERE id = NEW.id) THEN
-        SELECT IFNULL(MAX(id), 0) INTO v_max_id FROM document;
-        SET NEW.id = v_max_id + 1;
-    END IF;
-END//
-DELIMITER ;
-
-DELIMITER //
-
-
-
-CREATE TRIGGER avant_update_document
-    BEFORE UPDATE
-    ON document
-    FOR EACH ROW
-BEGIN
-    -- Empêche la modification du fichier lors de la mise à jour
-    IF (NEW.fichier <> OLD.fichier) OR ((NEW.fichier IS NULL) <> (OLD.fichier IS NULL)) THEN
-        SET NEW.fichier = OLD.fichier;
-    END IF;
-END//
-DELIMITER ;
-//
-
-DELIMITER ;
+# use ppe;
+#
+# drop trigger if exists avant_insert_document;
+# drop trigger if exists avant_update_document;
+#
+#
+#
+#
+# DELIMITER //
+# CREATE TRIGGER avant_insert_document
+#     BEFORE INSERT
+#     ON document
+#     FOR EACH ROW
+# BEGIN
+#     DECLARE v_max_id BIGINT;
+#
+#     -- Vérifie l'ID : si NULL ou existant, on prend max(id)+1
+#     IF NEW.id IS NULL OR EXISTS (SELECT 1 FROM document WHERE id = NEW.id) THEN
+#         SELECT IFNULL(MAX(id), 0) INTO v_max_id FROM document;
+#         SET NEW.id = v_max_id + 1;
+#     END IF;
+# END//
+# DELIMITER ;
+#
+# DELIMITER //
+#
+#
+#
+# CREATE TRIGGER avant_update_document
+#     BEFORE UPDATE
+#     ON document
+#     FOR EACH ROW
+# BEGIN
+#     -- Empêche la modification du fichier lors de la mise à jour
+#     IF (NEW.fichier <> OLD.fichier) OR ((NEW.fichier IS NULL) <> (OLD.fichier IS NULL)) THEN
+#         SET NEW.fichier = OLD.fichier;
+#     END IF;
+# END//
+# DELIMITER ;
+# //
+#
+# DELIMITER ;
