3dfaf088444f4f0169a0993c74ea2395e68f3ee2
LorenzoVsr <lorenzo.vasseur60@gmail.com>
2025-12-05 14:37:54 +0100

    Add files via upload

---
+++ inscription/index.php
@@ -0,0 +1,11 @@
+<?php
+// activation du chargement dynamique des ressources
+require_once __DIR__ . '/../include/autoload.php';
+
+$open = Inscription::getOpen();
+$titre = "Inscription";
+?>
+<?php
+require RACINE . "/include/interface.php";
+
+
